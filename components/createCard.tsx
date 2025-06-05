eaf164a0022b9d47f9ecc3906c71c213ce3a421a
Victor Santos <menezes.victorsantos@outlook.com>
2025-06-05 03:45:48 -0300

    Rendering button and fix input bugs

--- components/createCard.tsx
+++ components/createCard.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Card, Field, Flex, Input, Select, createListCollection, Portal } from "@chakra-ui/react";
+import { Card, Field, Flex, Input, Select, createListCollection, Portal, Dialog, Button, CloseButton } from "@chakra-ui/react";
 import { motion } from "motion/react";
 import { useEffect, useState } from "react";
 import { useCardContext } from "@/app/context/cardContext";
@@ -58,17 +58,11 @@ function CreateInput() {
 			w="full"
 			gapY="4"
 			flexWrap="wrap"
-			justifyContent="space-between"
-			initial={{ opacity: 0, scale: 0 }}
-			animate={{ opacity: 1, scale: 1 }}
-			transition={{
-				duration: 0.1,
-				scale: { type: "spring", visualDuration: 0.1, bounce: 0.1 },
-			}}>
+			justifyContent="space-between">
 			{selectedCard?.defaultInput.map((item) => {
 				return (
 					<Field.Root maxW="45%">
-						<Field.Label>{item.title}</Field.Label>
+						<Field.Label fontSize="md">{item.title}</Field.Label>
 						<Input type={item.type} size="xl" w="100%" />
 					</Field.Root>
 				);
@@ -77,7 +71,7 @@ function CreateInput() {
 				? selectedCard?.selectInput.map((item) => {
 						return (
 							<Select.Root collection={frameworks} maxW="45%" size="lg">
-								<Select.Label>{item.title}</Select.Label>
+								<Select.Label fontSize="md">{item.title}</Select.Label>
 								<Select.Control>
 									<Select.Trigger>
 										<Select.ValueText placeholder="Selecione" />
@@ -102,6 +96,44 @@ function CreateInput() {
 						);
 					})
 				: ""}
+			<Flex w="100%" justifyContent={"flex-end"}>
+				{selectedCard != undefined ? (
+					<Dialog.Root>
+						<Dialog.Trigger asChild>
+							<Button size="lg">Continuar</Button>
+						</Dialog.Trigger>
+						<Portal>
+							<Dialog.Backdrop />
+							<Dialog.Positioner>
+								<Dialog.Content>
+									<Dialog.Header>
+										<Dialog.Title>Confirmar criação</Dialog.Title>
+									</Dialog.Header>
+									<Dialog.Body>
+										<p>
+											Tem certeza que deseja criar a instâncias com os valores preenchidos? Certifique-se de ter preenchido os campos corretamente para evitar erros de consultar
+											no futuro.
+										</p>
+									</Dialog.Body>
+									<Dialog.Footer>
+										<Dialog.ActionTrigger asChild>
+											<Button variant="outline">Vou revisar</Button>
+										</Dialog.ActionTrigger>
+										<Dialog.ActionTrigger>
+											<Button onClick={() => {}}>Criar instância</Button>
+										</Dialog.ActionTrigger>
+									</Dialog.Footer>
+									<Dialog.CloseTrigger asChild>
+										<CloseButton size="sm" />
+									</Dialog.CloseTrigger>
+								</Dialog.Content>
+							</Dialog.Positioner>
+						</Portal>
+					</Dialog.Root>
+				) : (
+					""
+				)}
+			</Flex>
 		</MotionFlex>
 	);
 }
