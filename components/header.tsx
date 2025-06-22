ccbf0d7ffb2e5ee33bd20db5cb954f206f700a17
Victor Santos <menezes.victorsantos@outlook.com>
2025-06-22 02:08:21 -0300

    changed header design (in progress)

--- components/header.tsx
+++ components/header.tsx
@@ -1,75 +1,87 @@
 "use client";
-import { Flex, SegmentGroup, Button } from "@chakra-ui/react";
-import { useRouter, usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Avatar, Flex, Heading, Text, Menu, Portal, Button, Drawer, CloseButton, Icon } from "@chakra-ui/react";
 import { ColorModeButton } from "./ui/color-mode";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
+import { FaBolt } from "react-icons/fa6";
+import { GiHamburgerMenu } from "react-icons/gi";
 export default function Header() {
-	const path = usePathname();
-	const router = useRouter();
-	const logOut = async () => {
-		await signOut({
-			redirect: true,
-			callbackUrl: "/auth/login"
-		});
-	}
-	const [page, setPage] = useState<any>("");
-	const item = [
-		{
-			title: "Home",
-			route: "/user/home",
-		},
-		{
-			title: "Buscar",
-			route: "/user/search",
-		},
-		{
-			title: "Adicionar",
-			route: "/user/create",
-		},
-	];
-
-	useEffect(() => {
-		item.map((opt) => {
-			if (opt.route == path) {
-				setPage(opt.title);
-			}
-		});
-	}, []);
-
+	const { status, data } = useSession();
+	const handleDisconnect = async () => {
+		await signOut({ redirect: true, callbackUrl: "/auth/login" });
+	};
 	return (
 		<Flex
-			style={{width: "100%", maxWidth: "100%"}}
+			style={{ width: "100%", maxWidth: "100%" }}
 			padding="2"
 			bg={{ base: "#FAFAFA", _dark: "#09090b" }}
 			borderBottomColor={{ base: "#E4E4E7", _dark: "#323232" }}
 			borderBottomWidth="1px"
 			justifyContent="space-between"
 			alignItems="center">
-			Project v1.0
-			
-			<Flex gap={3} align={"center"} >
+			<Flex>
+				<Drawer.Root placement={"start"}>
+					<Drawer.Trigger asChild>
+						<Button variant="ghost" padding={1}>
+							<GiHamburgerMenu />
+						</Button>
+					</Drawer.Trigger>
+					<Portal>
+						<Drawer.Backdrop />
+						<Drawer.Positioner>
+							<Drawer.Content>
+								<Drawer.Header>
+									<Drawer.Title>Navegar</Drawer.Title>
+								</Drawer.Header>
+								<Drawer.Body>
+									<Flex direction={"column"}>
+										{Array.from({ length: 4 }).map((_, i) => {
+											return (
+												<Flex padding={3} align={"center"} gap={2} _hover={{ bg: "bg.emphasized", rounded: "md" }} cursor={"pointer"}>
+													<Icon size={"sm"}>
+														<FaBolt />
+													</Icon>
+													<Text textStyle={"md"}>Option {i}</Text>
+												</Flex>
+											);
+										})}
+									</Flex>
+								</Drawer.Body>
+								<Drawer.CloseTrigger asChild>
+									<CloseButton size="sm" />
+								</Drawer.CloseTrigger>
+							</Drawer.Content>
+						</Drawer.Positioner>
+					</Portal>
+				</Drawer.Root>
+			</Flex>
+			<Flex gap={3} align={"center"} maxW={"full"}>
 				<ColorModeButton />
-				<Button onClick={logOut} variant={"subtle"}>Sign Out</Button>
-				<SegmentGroup.Root value={page}>
-					<SegmentGroup.Indicator />
-					{item.map((option) => {
-						return (
-							<SegmentGroup.Item
-								value={option.title}
-								key={option.route}
-								cursor="pointer"
-								onClick={() => {
-									setPage(option.title);
-									setTimeout(() => {
-										router.push(option.route);
-									}, 200);
-								}}>
-								<SegmentGroup.ItemText>{option.title}</SegmentGroup.ItemText>
-							</SegmentGroup.Item>
-						);
-					})}
-				</SegmentGroup.Root>
+				{data?.user && (
+					<Menu.Root>
+						<Menu.Trigger asChild>
+							<Button variant={"ghost"} colorPalette={"black"} padding={1}>
+								<Flex align={"center"} gap={1}>
+									<Avatar.Root size={"xs"} scale={0.75}>
+										<Avatar.Image src={data.user.image as string} />
+									</Avatar.Root>
+								</Flex>
+							</Button>
+						</Menu.Trigger>
+						<Portal>
+							<Menu.Positioner>
+								<Menu.Content>
+									<Menu.ItemGroup>
+										<Menu.ItemGroupLabel>{data.user.name}</Menu.ItemGroupLabel>
+										<Menu.Separator />
+										<Menu.Item cursor={"pointer"} value="disconnect" color="fg.error" _hover={{ bg: "bg.error", color: "fg.error" }} onClick={handleDisconnect}>
+											Desconectar
+										</Menu.Item>
+									</Menu.ItemGroup>
+								</Menu.Content>
+							</Menu.Positioner>
+						</Portal>
+					</Menu.Root>
+				)}
 			</Flex>
 		</Flex>
 	);
